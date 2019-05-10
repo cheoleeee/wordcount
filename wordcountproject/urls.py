79eade6b85f9700bbe45c77001409f601b58d44a
cheoleeee <jhc2426@naver.com>
2019-05-10 19:12:48 +0900

    first commit

---
+++ wordcountproject/urls.py
@@ -0,0 +1,24 @@
+"""wordcountproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import wordcount.views  #views.py 파일안에 만들어 놓은 home 함수를 이용하기 위허서 Import
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',wordcount.views.index,name='index'),
+    path('result/',wordcount.views.result,name='result'), 
+]
